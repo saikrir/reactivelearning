359c803ee392bd37369bc3724634c2eba4457a34
skrao <rao.saikrishna@gmail.com>
2022-05-30 22:03:07 -0500

    Added FileReader Service

--- src/main/java/org/saikrishna/reactive/learning/ReactiveFileService.java
+++ src/main/java/org/saikrishna/reactive/learning/ReactiveFileService.java
@@ -1,2 +1,76 @@
-package org.saikrishna.reactive.learning;public class ReactiveFileService {
+package org.saikrishna.reactive.learning;
+
+import org.saikrishna.reactive.learning.service.FileReaderService;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.SynchronousSink;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class ReactiveFileService {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Path filePath = Paths.get("src/main/resources/", "Sample.txt");
+        //generate(filePath);
+        create(filePath);
+    }
+
+    private static void generate(Path path) {
+        fluxGenerated(path)
+                .subscribe(System.out::println,
+                        System.err::println,
+                        () -> System.out.println("Completed"));
+    }
+
+    private static void create(Path filePath) throws FileNotFoundException {
+        FileReaderService fileReaderService = new FileReaderService(filePath);
+
+
+        fluxCreated(fileReaderService)
+                .subscribe(o -> System.out.println(o),
+                        throwable -> System.out.println(throwable.getMessage()),
+                        () -> System.out.println("Completed"));
+
+
+        fileReaderService.readLines();
+    }
+
+    private static Flux<String> fluxGenerated(Path filePath) {
+        return Flux.generate(
+                initializeFileScanner(filePath),
+                readFileLines(),
+                closeFileScanner());
+    }
+
+    private static Flux<String> fluxCreated(FileReaderService fileReaderService) {
+        return Flux.create(fileReaderService);
+    }
+
+    private static Consumer<Scanner> closeFileScanner() {
+        return scanner -> {
+            System.out.println("FileStream will be closed");
+            scanner.close();
+        };
+    }
+
+    private static BiFunction<Scanner, SynchronousSink<Object>, Scanner> readFileLines() {
+        return (scanner, synchronousSink) -> {
+            if (scanner.hasNext()) {
+                synchronousSink.next(scanner.nextLine());
+            } else {
+                synchronousSink.complete();
+            }
+            return scanner;
+        };
+    }
+
+
+    private static Callable initializeFileScanner(Path filePath) {
+        return () -> new Scanner(filePath.toFile());
+    }
 }
--- src/main/java/org/saikrishna/reactive/learning/service/FileReaderService.java
+++ src/main/java/org/saikrishna/reactive/learning/service/FileReaderService.java
@@ -1,10 +1,37 @@
 package org.saikrishna.reactive.learning.service;
 
 import lombok.AllArgsConstructor;
+import reactor.core.publisher.FluxSink;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.function.Consumer;
 
 @AllArgsConstructor
-public class FileService {
+public class FileReaderService implements Consumer<FluxSink<String>> {
 
-    
+    private FluxSink<String> fluxSink;
 
+    private final Scanner scanner;
+
+    public FileReaderService(Path path) throws FileNotFoundException {
+        scanner = new Scanner(path.toFile());
+    }
+
+
+    @Override
+    public void accept(FluxSink<String> stringFluxSink) {
+        this.fluxSink = stringFluxSink;
+    }
+
+
+    public void readLines() {
+        while (scanner.hasNext()) {
+            this.fluxSink.next(scanner.nextLine());
+        }
+        this.fluxSink.complete();
+        this.scanner.close();
+        System.out.println("FileStream Closed");
+    }
 }
